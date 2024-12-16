fdc2ca40b35f2746484a8c6495eb8902e6fe4e6c
Antonio Pio Maggio <antoniopio.maggio@gmail.com>
2024-12-16 19:20:11 +0100

    WIP vehicle bar

--- src/app/components/vehicle-selector/vehicle-selector.component.ts
+++ src/app/components/vehicle-selector/vehicle-selector.component.ts
@@ -2,26 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Select } from 'primeng/select';
-import { FloatLabel } from 'primeng/floatlabel';
 import { VehicleService } from '../../services/vehicle.service';
+import { InputNumber } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-vehicle-selector',
   standalone: true,
-  imports: [CommonModule, FormsModule, Select, FloatLabel],
+  imports: [CommonModule, FormsModule, Select, InputNumber],
   providers: [VehicleService],
   templateUrl: './vehicle-selector.component.html',
   styleUrl: './vehicle-selector.component.scss'
 })
 export class VehicleSelectorComponent {
 
-  // public vehicles: any[] = [
-  //   { label: 'Car', value: 'car' },
-  //   { label: 'Bike', value: 'bike' },
-  //   { label: 'Truck', value: 'truck' }
-  // ];
-
   public selectedVehicle: object | undefined;
+  public tank: number = 100;
   public vehicles: any[] = [];
   constructor(private vehicleService: VehicleService) { }
 
